0cd6c2de86e41698317e688bba4d500895b990bb
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-22 12:00:52 +0200

    chore: back to build files

--- package.js
+++ package.js
@@ -19,11 +19,10 @@ Package.onUse(function(api) {
     'babel-compiler@6.8.0'
   ];
 
-  api.use("barbatus:typescript@0.3.3", ["client", "server"]);
   api.use(packages, 'server');
   api.imply(packages, 'server');
 
-  api.mainModule('src/index.ts', 'server');
+  api.mainModule('build/src/index.js', 'server');
 
   api.export([
     'StaticHtmlCompiler'
