048bca4909496398ab2b30328293d79b60a021f3
sebakerckhof <seba.kerckhof@gmail.com>
2018-09-24 17:02:52 +0200

    Update dependencies

--- package.js
+++ package.js
@@ -1,21 +1,21 @@
 Package.describe({
   name: 'urigo:static-html-compiler',
-  version: '1.0.0',
+  version: '1.1.0',
   summary: 'Compiles static HTML templates so you could import them from a module'
 });
 
 Npm.depends({
   'cheerio': '0.22.0',
-  'html-minifier': '3.5.8',
+  'html-minifier': '3.5.20',
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.6.1');
+  api.versionsFrom('METEOR@1.7.0.5');
 
   const packages = [
-    'caching-compiler@1.1.10',
-    'ecmascript@0.10.0',
-    'babel-compiler@7.0.0',
+    'caching-compiler@1.2.0',
+    'ecmascript@0.11.1',
+    'babel-compiler@7.2.0',
   ];
 
   api.use(packages, 'server');
